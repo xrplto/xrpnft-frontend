48b545444c0720735f74cfc08dcd635cf83448b0
Mahiros <95180295+mahiros-tech@users.noreply.github.com>
2022-02-02 05:54:20 +0300

    Token API implementation - 1

---
+++ server1/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+//const expressWs = require('express-ws');
+//const xrpl = require('./lib/xrpl-ws');
+//const streams = require('./lib/streams');
+const routes = require('./routes/v1');
+
+const log = require('./lib/logger')({ name: 'Server' });
+
+const PORT = process.env.PORT || 5001;
+const ADDR = process.env.ADDR || 'localhost';
+const app = express();
+const files = express.static(path.join(__dirname, '/../build'));
+
+/*
+app.use('*', (req, res, next) => {
+  log.info(req.originalUrl);
+  next();
+});
+*/
+
+express(app);
+app.use(bodyParser.json());
+//app.use(files);
+//app.ws('/ws', ws => streams.addWs(ws));
+app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+  });
+});
+
+//xrpl.start();
+app.listen(PORT, ADDR);
+log.info(`Server listening on ${ADDR}:${PORT}`);
