9dcaf25b614ee901ca7e56871517e50d32c0fd19
Mahiros <mahiros.tech@gmail.com>
2022-12-10 16:47:07 +0200

    Icon changes

--- pages/_app.js
+++ pages/_app.js
@@ -39,11 +39,13 @@ function XRPNFTApp(props) {
                     <meta name="theme-color" content="#ffffff"/>
                 */}
 
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="apple-touch-icon" sizes="192x192" href="/icons/apple-icon.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-96x96.png" />
+                
                 <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#121619" />
+                {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#121619" /> */}
                 <meta name="msapplication-TileColor" content="#121619" />
                 <meta name="theme-color" content="#ffffff"/>
 
