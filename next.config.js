bb5d36f815dbf4a3f2819de0be668ce7eb5148ee
cool725 <109250241+cool725@users.noreply.github.com>
2023-05-23 14:19:34 +0200

    fixed static page generation timeout error

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 const withImages = require('next-images');
-module.exports = withImages()
+module.exports = withImages();
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //     enabled: process.env.ANALYZE === 'true',
 // })
-  
+
 // module.exports = withBundleAnalyzer({ })
 
-const isProd = process.env.RUN_ENV === 'production'
+const isProd = process.env.RUN_ENV === 'production';
 
 module.exports = {
-	images: {
-    	domains: ['s1.xrpnft.com'],
-  	},
-    poweredByHeader: false,
-    // assetPrefix: isProd ? 'https://xrpnft.com' : '',
-}
+  images: {
+    domains: ['s1.xrpnft.com']
+  },
+  poweredByHeader: false,
+  // assetPrefix: isProd ? 'https://xrpnft.com' : '',
+	staticPageGenerationTimeout: 1200000
+};
