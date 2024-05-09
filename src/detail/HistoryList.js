b32baa250e985b26e16dd60cf06189b9c65d9f1d
xrplto <93736363+xrplto@users.noreply.github.com>
2024-05-09 13:58:49 -0400

    Update HistoryList.js
    Made it more human readable.

--- src/detail/HistoryList.js
+++ src/detail/HistoryList.js
@@ -1,56 +1,60 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 // Material
 import {
     useTheme,
-    Avatar,
-    Backdrop,
     Box,
-    Divider,
-    IconButton,
     Link,
     Stack,
     Table,
     TableBody,
     TableCell,
     TableRow,
-    Tooltip,
     Typography
 } from '@mui/material';
 import { tableCellClasses } from "@mui/material/TableCell";
 
 // Loader
-import { PuffLoader, PulseLoader } from "react-spinners";
-import { ProgressBar, Discuss } from 'react-loader-spinner';
-
-// Utils
-import { formatDateTime } from 'src/utils/formatTime';
-import { normalizeCurrencyCodeXummImpl } from "src/utils/normalizers";
+import { PulseLoader } from "react-spinners";
 
 // Context
-import { useContext } from 'react';
 import { AppContext } from 'src/AppContext';
 
-// Components
+// Utils
+import { normalizeCurrencyCodeXummImpl } from "src/utils/normalizers";
 
 function truncate(str, n) {
     if (!str) return '';
-    //return (str.length > n) ? str.substr(0, n-1) + '&hellip;' : str;
     return (str.length > n) ? str.substr(0, n-1) + ' ...' : str;
 };
+
+function formatDateTime(dateString) {
+    const date = new Date(dateString);
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZone: 'America/New_York',
+        timeZoneName: 'short'
+    };
+    return date.toLocaleString('en-US', options);
+}
+
+function formatAmount(amount) {
+    return parseFloat(amount).toFixed(1);
+}
 
 export default function HistoryList({ nft }) {
     const theme = useTheme();
     const BASE_URL = 'https://api.xrpnft.com/api';
-    const { accountProfile, openSnackbar, sync, setSync } = useContext(AppContext);
-    const accountLogin = accountProfile?.account;
-    const accountToken = accountProfile?.token;
-
-    const isOwner = accountLogin === nft.account;
-
+    const { accountProfile, sync } = useContext(AppContext);
     const [hists, setHists] = useState([]);
-
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -61,12 +65,10 @@ export default function HistoryList({ nft }) {
                     let ret = res.status === 200 ? res.data : undefined;
                     if (ret) {
                         setHists(ret.histories);
-                        // console.log(ret.histories);
                     }
+                    setLoading(false);
                 }).catch(err => {
-                    console.log("Error on getting nft history list!!!", err);
-                }).then(function () {
-                    // always executed
+                    console.error("Error on getting nft history list!!!", err);
                     setLoading(false);
                 });
         }
@@ -101,79 +103,28 @@ export default function HistoryList({ nft }) {
                             }
                         }}>
                             <TableBody>
-                            {
-                                hists && hists.map((row, id) => {
-                                    const {
-                                        uuid,
-                                        type,
-                                        account,
-                                        NFTokenID,
-                                        URI,
-                                        cost,
-                                        self,
-                                        ledger,
-                                        hash,
-                                        time
-                                    } = row;
-
-                                    let strDateTime = formatDateTime(time);
-
-                                    return (
-                                        <TableRow
-                                            // hover
-                                            key={uuid}
-                                            sx={{
-                                                [`& .${tableCellClasses.root}`]: {
-                                                    // color: (error ? '#B72136' : '#B72136')
-                                                }
-                                            }}
-                                        >
-                                            <TableCell align="left" width='8%' sx={{pt:0.5, pb:0.5}}>
-                                                <Typography variant="s7">{id+1}</Typography>
-                                            </TableCell>
-                                            <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
-                                                <Typography variant='s11' noWrap>{type}</Typography>
-                                            </TableCell>
-                                            <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
-                                                <Link
-                                                    // color="inherit"
-                                                    // target="_blank"
-                                                    href={`/account/${account}`}
-                                                    // rel="noreferrer noopener nofollow"
-                                                >
-                                                    <Typography variant='s11' noWrap> {truncate(account, 16)}</Typography>
-                                                </Link>
-                                            </TableCell>
-                                            <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
-                                                {type === 'SALE' ?
-                                                    <Typography variant='s11' noWrap>{cost.amount} {normalizeCurrencyCodeXummImpl(cost.currency)}</Typography>
-                                                    :
-                                                    <Typography variant='s11' noWrap>- - -</Typography>
-                                                }
-
-                                            </TableCell>
-                                            <TableCell align="left" sx={{pt:0.5, pb:0.5}}>
-                                                <Typography variant='s7' noWrap>{strDateTime}</Typography>
-                                            </TableCell>
-                                            <TableCell align="left" width='3%' sx={{pt:0.5, pb:0.5}}>
-                                                <Link
-                                                    underline="none"
-                                                    color="inherit"
-                                                    target="_blank"
-                                                    href={`https://bithomp.com/explorer/${hash}`}
-                                                    rel="noreferrer noopener nofollow"
-                                                >
-                                                    <Tooltip title="Check Tx on Bithomp">
-                                                        <IconButton edge="end" aria-label="bithomp" size="small">
-                                                            <Avatar alt="bithomp" src="/static/bithomp.ico" sx={{ width: 16, height: 16 }} />
-                                                        </IconButton>
-                                                    </Tooltip>
-                                                </Link>
-                                            </TableCell>
-                                        </TableRow>
-                                    );
-                                })
-                            }
+                                {hists && hists.map((row) => (
+                                    <TableRow key={row.uuid}>
+                                        <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
+                                            <Typography variant='body2' noWrap>{row.type}</Typography>
+                                        </TableCell>
+                                        <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
+                                            <Link href={`/account/${row.account}`}>
+                                                <Typography variant='body2' noWrap>{truncate(row.account, 16)}</Typography>
+                                            </Link>
+                                        </TableCell>
+                                        <TableCell align="left" width='15%' sx={{pt:0.5, pb:0.5}}>
+                                            {row.type === 'SALE' ?
+                                                <Typography variant='body2' noWrap>{formatAmount(row.cost.amount)} {normalizeCurrencyCodeXummImpl(row.cost.currency)}</Typography>
+                                                :
+                                                <Typography variant='body2' noWrap>- - -</Typography>
+                                            }
+                                        </TableCell>
+                                        <TableCell align="left" sx={{pt:0.5, pb:0.5}}>
+                                            <Typography variant='body2' noWrap>{formatDateTime(row.time)}</Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
                             </TableBody>
                         </Table>
                     </Box>
