f8406c6fc25ae34879a762126e0637d1d2d325a3
cool725 <109250241+cool725@users.noreply.github.com>
2024-05-17 02:19:50 +0700

    update NFTActions getPayload

--- src/detail/NFTActions.js
+++ src/detail/NFTActions.js
@@ -246,36 +246,7 @@ export default function NFTActions({ nft }) {
             return dispatched_result;
           } catch (err) {}
         }
-
-        const startInterval = () => {
-          let times = 0;
     
-          dispatchTimer = setInterval(async () => {
-            const dispatched_result = await getDispatchResult();
-    
-            if (dispatched_result && dispatched_result === 'tesSUCCESS') {
-              setSync(sync + 1);
-              openSnackbar('Successful!', 'success');
-              stopInterval();
-              return;
-            }
-    
-            times++;
-    
-            if (times >= 10) {
-              openSnackbar('Rejected!', 'error');
-              stopInterval();
-              return;
-            }
-          }, 1000);
-        };
-    
-        // Stop the interval
-        const stopInterval = () => {
-          clearInterval(dispatchTimer);
-          handleScanQRClose();
-        };
-
         async function getPayload() {
             console.log(counter + " " + isRunning, xummUuid);
             if (isRunning) return;
@@ -285,8 +256,18 @@ export default function NFTActions({ nft }) {
                 const resolved_at = ret.data?.resolved_at;
                 // const dispatched_result = ret.data?.dispatched_result;
                 if (resolved_at) {
-                    startInterval();
-                    return;
+                    const dispatched_result = await getDispatchResult();
+    
+                    if (dispatched_result) {
+                        if (dispatched_result === 'tesSUCCESS') {
+                            setSync(sync + 1);
+                            openSnackbar('Successful!', 'success');
+                        } else {
+                            openSnackbar('Rejected!', 'error');
+                        }
+                        handleScanQRClose();
+                        return;
+                    }
                 }
             } catch (err) {
             }
