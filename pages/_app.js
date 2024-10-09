70949ce5cbf7e1c09bb9f3da2c1e816531c88a53
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-10-09 13:21:06 -0400

    meta update

--- pages/_app.js
+++ pages/_app.js
@@ -17,61 +17,55 @@ function XRPNFTApp(props) {
     const ogp = pageProps.ogp || {};
     const data = pageProps.data;
 
-    //ogp.type = ogp.isVideo ? "video" : "image"; // image should always be
+    // Ensure title and description are always set
+    const title = ogp.title ? `${ogp.title} | XRPNFT` : 'XRPNFT - XRP NFT Marketplace';
+    const description = ogp.desc || 'Discover and trade unique NFTs on the XRP Ledger with XRPNFT, the premier marketplace for digital collectibles.';
 
     return (
         <>
             <Head>
+                <meta charSet="utf-8" />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
                 <meta name="google-site-verification" content="dDLV5BZHngydfQXYGuUX34wnShMPqM-f5dHS2BRVdsQ" />
 
-                {/*
-                    <meta name="apple-mobile-web-app-title" content="Snippit"/>
-                    <meta name="application-name" content="<APP NAME>"/>
-                    <meta name="msapplication-TileColor" content="#ffc40d"/>
-                    <meta name="theme-color" content="#ffffff"/>
-                */}
-
                 <link rel="apple-touch-icon" sizes="192x192" href="/icons/apple-icon.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
 
                 <link rel="manifest" href="/site.webmanifest" />
-                {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#121619" /> */}
                 <meta name="msapplication-TileColor" content="#121619" />
                 <meta name="theme-color" content="#ffffff"/>
 
-                {/* <meta name="robots" content="nofollow"/> */}
-
-                {/* <link rel="preload" as="image" href="/static/collection/NFT_Labs_Images4.png"/> */}
-
-                <link rel="canonical" href={ogp.canonical}/>
+                <link rel="canonical" href={ogp.canonical || 'https://xrpnft.com'}/>
 
                 {/* <!-- HTML Meta Tags --> */}
-                <title>{ogp.title} | XRPNFT</title>
-                <meta name="description" content={ogp.desc}/>
+                <title>{title}</title>
+                <meta name="description" content={description}/>
 
-                {/* <!-- Facebook Meta Tags --> */}
-                <meta property="og:url" content={ogp.url}/>
+                {/* <!-- Open Graph / Facebook Meta Tags --> */}
                 <meta property="og:type" content="website"/>
-                <meta property="og:title" content={`${ogp.title} | XRPNFT`}/>
-                <meta property="og:description" content={ogp.desc}/>
-                <meta property="og:image" content={ogp.imgUrl}/>
-                <meta property="og:video" content={ogp.videoUrl || ""}/>
+                <meta property="og:url" content={ogp.url || 'https://xrpnft.com'}/>
+                <meta property="og:title" content={title}/>
+                <meta property="og:description" content={description}/>
+                <meta property="og:image" content={ogp.imgUrl || 'https://xrpnft.com/default-og-image.jpg'}/>  
+                {ogp.videoUrl && <meta property="og:video" content={ogp.videoUrl}/>}
+
                 {/* <!-- Twitter Meta Tags --> */}
                 <meta name="twitter:card" content="summary_large_image"/>
                 <meta property="twitter:domain" content="xrpnft.com"/>
-                <meta property="twitter:url" content={ogp.url}/>
-                <meta name="twitter:title" content={`${ogp.title} | XRPNFT`}/>
-                <meta name="twitter:description" content={ogp.desc}/>
-                {/* <!-- <meta name="twitter:image" content="/static/ogp.png"/> --> */}
-                <meta name={`twitter:${ogp.type}`} content={ogp.imgUrl}/>
-                <meta name={`twitter:${ogp.type}:src`} content={ogp.imgUrl}/>
-                {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
+                <meta property="twitter:url" content={ogp.url || 'https://xrpnft.com'}/>
+                <meta name="twitter:title" content={title}/>
+                <meta name="twitter:description" content={description}/>
+                <meta name="twitter:image" content={ogp.imgUrl || 'https://xrpnft.com/default-og-image.jpg'}/>  
+
+                {/* Additional SEO meta tags */}
+                <meta name="keywords" content="XRP, NFT, marketplace, digital collectibles, blockchain, cryptocurrency"/>
+                <meta name="author" content="XRPNFT"/>
+                <meta name="robots" content="index, follow"/>
             </Head>
             <ContextProvider data={data} openSnackbar={openSnackbar}>
                 <ThemeProvider>
