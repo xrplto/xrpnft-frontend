9e67d33f5a3aff5a84a741182a4bd56f5b046ce5
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-10-13 07:19:20 -0400

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,18 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     {/* Google Fonts links have been removed */}
+                    {/* Google tag (gtag.js) */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-E0TTF6N1W3"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-E0TTF6N1W3');
+                            `
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
